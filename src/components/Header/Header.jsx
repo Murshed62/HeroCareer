5cfbdd8496322ebd21591b3ae37ff6b2c2850d49
Murshed62 <shesir.murshed@gmail.com>
2023-04-12 23:03:41 +0600

    some design added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,17 @@
 import { Link } from 'react-router-dom';
-
+import './Header.css'
 
 const Header = () => {
 
     return (
         <>
       <nav className="md:flex md:items-center md:justify-between md:mx-20 mt-5">
-      <div className=" mb-4 header-title ">HeroCareer</div>
+      <div className=" mb-4 header-title res-design-logo">HeroCareer</div>
       <div className="inlineCss mb-4 text-2xl pl-5  md:flex md:gap-10 ">
-        <Link className='inlineCss mb-2 '  to='/home'>Home</Link>
-        <Link  to="/satatics" className='inlineCss mb-2'>Statistics</Link>
-        <Link  to="/applyJob" className='inlineCss mb-2'>Applied Jobs</Link>
-        <Link  to="/blog" className='inlineCss'>Blog</Link>
+        <Link className='inlineCss mb-2 nav-link'  to='/home'>Home</Link>
+        <Link to="/satatics" className='inlineCss mb-2 nav-link'>Statistics</Link>
+        <Link  to="/applyJob" className='inlineCss mb-2 nav-link'>Applied Jobs</Link>
+        <Link  to="/blog" className='inlineCss nav-link'>Blog</Link>
       </div>
       <button className="btn-primary btn-navbar font-bold py-2 px-4 rounded">
       Star Applying
